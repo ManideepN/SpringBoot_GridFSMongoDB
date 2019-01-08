5a7484fd3c7035a2c7059b95d687e816a3e78670
Manideep <Manideep.nagapuri@wipro.com>
2019-01-08 17:33:18 +0530

    updated

---
+++ src/main/java/application/AlgosApplication.java
@@ -0,0 +1,15 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgosApplication.class, args);
+		System.out.println("Started");
+	}
+
+}
+
